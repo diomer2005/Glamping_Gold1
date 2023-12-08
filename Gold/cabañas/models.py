6308889e9df241f8b1147a57f08b8a92b9477617
diomer2005 <diomer.quiroz.diossa@iemariacano.edu.co>
2023-12-07 20:36:05 -0500

    TERMINADO_OFICIAL

--- Gold/cabañas/models.py
+++ Gold/cabañas/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from servicios.models import Servicio
+from comodidades.models import Comodidad
+from tipocabañas.models import Tipocabaña
+
 
 class Cabaña(models.Model):
     nombre = models.CharField(max_length=25)
     capacidad = models.IntegerField()
     descripcion = models.CharField(max_length=25)
     status = models.BooleanField(default=True)
+    comodidades = models.ForeignKey('comodidades.Comodidad', on_delete=models.DO_NOTHING, null=True)
+    tipocabaña = models.ForeignKey('tipocabañas.Tipocabaña', on_delete=models.DO_NOTHING, null=True)
 
 # Create your models here
